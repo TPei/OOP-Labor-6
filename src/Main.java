6ecf3a8496d6f43c89f192dbcc2a532025653de6
dragon5689 <dragon5689@gmx.net>
2013-05-14 15:00:04 +0200

    added base files

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+	/**create a GameFrame object
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		new MyMiniGameWindow();
+
+	}
+
+}
---
+++ src/MyMiniGameWindow.java
@@ -0,0 +1,21 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import MiniGamePackage.MiniGameObserver;
+
+/**
+ * creates MiniGameWindow and its contents
+ * @author Thomas
+ *
+ */
+public class MyMiniGameWindow extends JFrame
+{
+	JButton upButton = new JButton("up");
+	JButton downButton = new JButton("down");
+	JButton moveButton = new JButton("move");
+	JButton startButton = new JButton("start");
+	
+	public MyMiniGameWindow()
+	{
+		
+	}
+}
