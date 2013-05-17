84497fddf43857a8d8aadf26188b44909edd62cf
dragon5689 <dragon5689@gmx.net>
2013-05-17 14:38:31 +0200

    created mini game

--- src/MyMiniGame.java
+++ src/MyMiniGame.java
@@ -4,74 +4,162 @@ import MiniGamePackage.Sprite;
 public class MyMiniGame extends MiniGame
 {
 
-	@Override
-	protected void computerMove(Action arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	}
+    final int NR_OF_FIELDS = 8;
 
-	@Override
-	protected void gameHasFinished()
-	{
-		// TODO Auto-generated method stub
-		
-	}
+    // 0: not set, 1: computer, 2: player
+    int[] fieldStatus = new int[NR_OF_FIELDS]; 
 
-	@Override
-	protected void gameHasStarted()
-	{
-		// TODO Auto-generated method stub
-		
-	}
+    Sprite[] fieldSprites = getSprites(0);
+    Sprite[] computerStoneSprites = getSprites(3);
+    Sprite[] playerStoneSprites = getSprites(4);
 
-	@Override
-	public int getCurrentComputerScore()
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    Sprite computerSprite = getSprite(1, 0);
+    Sprite playerSprite = getSprite(2, 0);
 
-	@Override
-	public int getCurrentPlayerScore()
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    int playerPosition = 0;
+    int computerPosition = 0;
 
-	@Override
-	public String getName()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public MyMiniGame()
+    {
+		getBackgroundPicture().paintRectangle(0, 0, 640, 640, -1, 255, 255, 0);
+	
+		fieldSprites[0].paintRectangle(0, 0, 32, 32, -1, 255, 255, 255);
+		fieldSprites[0].paintEllipse(5, 5, 22, 22, -1, 0, 0, 0);
+	
+		playerStoneSprites[0].paintEllipse(5, 5, 22, 22, -1, 255, 128, 0);
+		computerStoneSprites[0].paintEllipse(5, 5, 22, 22, -1, 0, 0, 255);
+	
+		computerSprite.paintRectangle(5, 0, 22, 32, -1, 0, 0, 255);
+		playerSprite.paintRectangle(5, 0, 22, 32, -1, 255, 128, 0);
 
-	@Override
-	public int getNrOfPlayerGoActionsMax(int arg0)
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    }
 
-	@Override
-	public int getNrofComputerGoActions(int arg0)
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    protected void initGame()
+    {
+		for (int i = 0; i < NR_OF_FIELDS; i++)
+		{
+		    fieldSprites[i].setPosition(304, i * 40 + 100);
+		    playerStoneSprites[i].setPosition(304, i * 40 + 100);
+		    computerStoneSprites[i].setPosition(304, i * 40 + 100);
+		    playerStoneSprites[i].dontShow();
+		    computerStoneSprites[i].dontShow();
+	
+		    fieldStatus[i] = 0;
+		}
+	
+		playerPosition = 0;
+		computerPosition = 0;
+		updatePositions();
+    }
 
-	@Override
-	protected void initGame()
-	{
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    protected void gameHasStarted()
+    {
+    }
 
-	@Override
-	protected void playerMove(Action arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    protected void computerMove(Action action)
+    {
+		switch (action)
+		{
+		case DOWN:
+		    computerPosition = Math.min(computerPosition + 1, NR_OF_FIELDS - 1);
+		    updatePositions();
+		    break;
+		case UP:
+		    computerPosition = Math.max(computerPosition - 1, 0);
+		    updatePositions();
+		    break;
+		case GO:
+		    fieldStatus[computerPosition] = 1;
+		    playerStoneSprites[computerPosition].dontShow();
+		    computerStoneSprites[computerPosition].show();
+		    break;
+		default:
+		    break;
+		}
+    }
+
+    @Override
+    protected void playerMove(Action action)
+    {
+		switch (action)
+		{
+		case DOWN:
+		    playerPosition = Math.min(playerPosition + 1, NR_OF_FIELDS - 1);
+		    updatePositions();
+		    break;
+		case UP:
+		    playerPosition = Math.max(playerPosition - 1, 0);
+		    updatePositions();
+		    break;
+		case GO:
+		    fieldStatus[playerPosition] = 2;
+		    playerStoneSprites[playerPosition].show();
+		    computerStoneSprites[playerPosition].dontShow();
+	
+		    break;
+		default:
+		    break;
+		}
+    }
+
+    @Override
+    protected void gameHasFinished()
+    {
+    }
+
+    @Override
+    public int getCurrentComputerScore()
+    {
+		int cs = 0;
+		for (int i = 0; i < fieldStatus.length; i++)
+		{
+		    if (fieldStatus[i] == 1)
+		    {
+		    	++cs;
+		    }
+		}
+		return cs * 100;
+    }
+
+    @Override
+    public int getCurrentPlayerScore()
+    {
+		int ps = 0;
+		for (int i = 0; i < fieldStatus.length; i++)
+		{
+		    if (fieldStatus[i] == 2)
+		    {
+		    	++ps;
+		    }
+		}
+		return ps * 100;
+    }
+
+    @Override
+    public String getName()
+    {
+    	return "My great game!";
+    }
+
+    @Override
+    public int getNrOfPlayerGoActionsMax(int difficulty)
+    {
+    	return 10;
+    }
+
+    @Override
+    public int getNrofComputerGoActions(int difficulty)
+    {
+    	return difficulty * 10;
+    }
+
+    private void updatePositions()
+    {
+		computerSprite.setPosition(200, computerPosition * 40 + 100);
+		playerSprite.setPosition(400, playerPosition * 40 + 100);
+    }
 
 }
--- src/MyMiniGameWindow.java
+++ src/MyMiniGameWindow.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,11 +13,11 @@ import MiniGamePackage.MiniGameObserver;
  * @author Thomas
  *
  */
-public class MyMiniGameWindow extends JFrame
+public class MyMiniGameWindow extends JFrame implements ActionListener, MiniGameObserver
 {
 	JButton upButton = new JButton("up");
 	JButton downButton = new JButton("down");
-	JButton moveButton = new JButton("move");
+	JButton fireButton = new JButton("bam");
 	JButton startButton = new JButton("start");
 	
 	MyMiniGame theGame = new MyMiniGame();
@@ -24,18 +26,54 @@ public class MyMiniGameWindow extends JFrame
 	{
 		setSize(640, 640);
 		setLayout(new BorderLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		// add buttons to toolbar
 		JToolBar toolbar = new JToolBar();
 		add(toolbar, BorderLayout.NORTH);
 		toolbar.add(upButton);
 		toolbar.add(downButton);
-		toolbar.add(moveButton);
+		toolbar.add(fireButton);
 		toolbar.add(startButton);
 		
+		// add action listeners for buttons
+		upButton.addActionListener(this);
+		downButton.addActionListener(this);
+		fireButton.addActionListener(this);
+		startButton.addActionListener(this);
 		
-		
-		
+		add(theGame, BorderLayout.CENTER);
+		theGame.registerMiniGameObserver(this);
 		
 		setVisible(true);
+	}
+
+	@Override
+	public void gameStatusUpdate(int remainingTime, int playerScore, int computerScore, boolean isRunning)
+	{
+		// TODO Auto-generated method stub
+		setTitle("Time left: " + remainingTime + " Player: " + playerScore + "  Computer: " + computerScore + " Still running: " + isRunning);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		// action events for pressed buttons
+		if(e.getSource() == upButton)
+		{
+			theGame.playerActionUp();
+		}
+		else if(e.getSource() == downButton)
+		{
+			theGame.playerActionDown();
+		}
+		else if(e.getSource() == fireButton)
+		{
+			theGame.playerActionGo();
+		}
+		else if(e.getSource() == startButton)
+		{
+			theGame.newGame(1);
+		}
 	}
 }
